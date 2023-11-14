71ed06c7da027ded8f47216081cc166e9bedcdfd
claralegorburu <134685191+claralegorburu@users.noreply.github.com>
2023-11-14 16:41:16 +0100

    Search functionality using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function getWeather(response) {
+  let temperatureElement = document.querySelector("#weather-app-temp");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "dd951o2a57d4a5945e3bta7c8a72f3a0";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(getWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("New York");
